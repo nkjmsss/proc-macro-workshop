c9bf3f66cf6c215c64ce7e78458816034488232f
nkjmsss <tempest0302@gmail.com>
2022-01-13 16:10:11 +0900

    progress-1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,13 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let expanded = quote! {};
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(expanded)
 }
