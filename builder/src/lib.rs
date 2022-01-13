792fbc862c1103776c860890a41ac6ebfdeac917
nkjmsss <tempest0302@gmail.com>
2022-01-13 16:49:25 +0900

    progress-4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,8 +10,13 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let builder_name = Ident::new(&format!("{}Builder", target_name), Span::call_site());
 
     let (field_name, field_type) = parse_field(&input.data);
+
+    // target impl
     let builder_impl = builder_impl(&input.data, &builder_name);
+
+    // builder impl
     let setter_impl = setter_impl(&input.data);
+    let build_impl = build_impl(&input.data, target_name);
 
     let expanded = quote! {
         pub struct #builder_name {
@@ -26,6 +31,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
         impl #builder_name {
             #setter_impl
+            #build_impl
         }
     };
 
@@ -76,5 +82,29 @@ fn setter_impl(data: &syn::Data) -> TokenStream {
                 self
             }
         )*
+    }
+}
+
+fn build_impl(data: &syn::Data, target_name: &Ident) -> TokenStream {
+    let (field_name, _) = parse_field(data);
+    let field_name2 = field_name.clone();
+    let err_msg = field_name.clone().into_iter().map(|v| {
+        if let Some(v) = v {
+            Some(format!("'{}' not set", v.to_string()))
+        } else {
+            None
+        }
+    });
+
+    quote! {
+        pub fn build(&mut self) -> std::result::Result<#target_name, Box<dyn std::error::Error>> {
+            std::result::Result::Ok(
+                #target_name {
+                    #(
+                        #field_name: self.#field_name2.clone().ok_or(#err_msg)?,
+                    )*
+                }
+            )
+        }
     }
 }
